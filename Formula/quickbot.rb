5c335ae344d50aff85c50644d491ced7ccf43db8
levin <levin.mircea@gmail.com>
2026-01-05 23:19:05 +0200

    Update QuickBot formula description and installation process

--- Formula/quickbot.rb
+++ Formula/quickbot.rb
@@ -1,22 +1,26 @@
 class Quickbot < Formula
-  desc "QuickBot CLI - plugin-enabled automation tool"
+  desc "Quick-Bot CLI - Automation & plugin management"
   homepage "https://github.com/levinismynameirl/Quick-Bot"
   url "https://github.com/levinismynameirl/Quick-Bot/archive/refs/tags/v1.0.1.1.tar.gz"
   sha256 "893f13f31767bd80de21f93d8c10ae6657d582ec295589fa4662e928d6c89938"
   license "MIT"
 
-  depends_on "python@3.11"
-
   def install
+    # Install everything into the prefix directory
     prefix.install Dir["*"]
-    bin.install "install.sh" => "quick"
+
+    # Make install.sh executable
+    chmod "+x", "#{prefix}/install.sh"
+
+    # Optionally, run the install.sh to finish installation
+    system "#{prefix}/install.sh"
+
+    # Symlink quick command into Homebrew bin
+    bin.install "#{prefix}/quick" => "quick"
   end
 
-  def caveats
-    <<~EOS
-      QuickBot CLI installed!
-      Run: quick --help
-      To install plugins: quick plugin --install <plugin>
-    EOS
+  test do
+    # Basic sanity check
+    assert_match "Quick-Bot", shell_output("#{bin}/quick --version")
   end
 end
